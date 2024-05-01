57efac0f91c8a6af29fbe85e66e8d4eebf307c71
Aksel Binderup Kristensen <APT@kmd.dk>
2024-05-01 10:27:02 +0200

    Oprettet repository til produkt, oprettet CommonDBModule til at gøre DB-værk lettere overskueligt og oprettet et interface til CRUD.

---
+++ ERP/Database/CommonDBModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace ERP;
+
+public partial class CommonDBModule
+{
+    public SqlConnection GetConnection()
+    {
+        SqlConnection sqlConnection = new SqlConnection("Indsæt connstræng");
+        sqlConnection.Open();
+        return sqlConnection;
+    }
+
+    public bool ExecuteCommand()
+    {
+        //laver lige en hurtig execute senere
+        return true;
+    }
+}
---
+++ ERP/Database/ProductRepository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP;
+
+public class ProductRepository : IDBrepository<Produkt>
+{
+    public bool Create(Produkt obj)
+    {
+        throw new NotImplementedException();
+    }
+    public bool Delete(Produkt obj)
+    {
+        throw new NotImplementedException();
+    }
+    public bool Read(Produkt obj)
+    {
+        throw new NotImplementedException();
+    }
+    public bool Update(Produkt obj)
+    {
+        throw new NotImplementedException();
+    }
+}
