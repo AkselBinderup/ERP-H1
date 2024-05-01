f6b36d8b8f7ad181258455ce312bf91465d8861b
victoref <victoref3@gmail.com>
2024-05-01 10:46:07 +0200

    S1 Tomme Metoder

--- ERP/Domain model/Entiteter/Salgsordrehoved.cs
+++ ERP/Domain model/Entiteter/Salgsordrehoved.cs
@@ -14,9 +14,27 @@ public class Salgsordrehoved
         Pakket,
         Færdig
     }
+    Tilstand Tilstand_ { get; set; }
     public List<SalgsOrdreLinje> OrdreLinjer { get; set; }
 
     float Ordrebeløb {  get; set; }
+
+    public Salgsordrehoved(int ordrenummer,
+                           DateTime oprettelsetid,
+                           DateTime Gennemførttid,
+                           int kodenummer,
+                           Tilstand tilstand,
+                           float ordrebeløb)
+    {
+        
+        OrdreNummer = ordrenummer;
+        OprettelsesTidspunkt = oprettelsetid;
+        GennemførelsesTidspunkt = Gennemførttid;
+        KundeNummer = kodenummer;
+        Tilstand_ = tilstand;
+        Ordrebeløb = ordrebeløb;
+
+    }
 
 
 }
