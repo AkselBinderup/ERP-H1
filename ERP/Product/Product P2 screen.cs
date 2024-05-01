cc6ca0dbad8b70d49bc8af7df8f8d01f8bda5971
victoref <victoref3@gmail.com>
2024-05-01 10:11:36 +0200

    D6 D7 begyndt på S1

--- ERP/Product/Product P2 screen.cs
+++ ERP/Product/Product P2 screen.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ERP;
-
-public partial class Product
+namespace ERP
 {
+    public partial class ProductListe
+    {
+    }
 }
