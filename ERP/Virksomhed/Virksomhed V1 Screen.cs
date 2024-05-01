829861b16e8c8ee7a4db56ce9d560ca1299c1dd3
victoref <victoref3@gmail.com>
2024-05-01 15:21:05 +0200

    P2 Påbegundt

--- ERP/Virksomhed/Virksomhed V1 Screen.cs
+++ ERP/Virksomhed/Virksomhed V1 Screen.cs
@@ -4,32 +4,37 @@ namespace ERP;
 public partial class VirksomhedStart : Screen
 {
     public override string Title { get; set; } = "Vælg virksomhed";
-
+    
     protected override void Draw()
     {
         ExitOnEscape();
 
         Console.CursorVisible = false;
 
-        ListPage<Virksomhed> side = new ListPage<Virksomhed>();
-        side.AddColumn("Firmanavn", nameof(Virksomhed.FirmaNavn), 40);
-        side.AddColumn("Land", nameof(Virksomhed.Land));
-        side.AddColumn("Valuta", nameof(Virksomhed.Valuta), 8);
+        ListPage<Virksomhed> listPage = new ListPage<Virksomhed>();
+        listPage.AddColumn("Firmanavn", nameof(Virksomhed.FirmaNavn), 40);
+        listPage.AddColumn("Land", nameof(Virksomhed.Land));
+        listPage.AddColumn("Valuta", nameof(Virksomhed.Valuta), 8);
 
         TempDataBase db = new TempDataBase();
-        var virksomhedder = db.GetData();
-        foreach (Virksomhed model in virksomhedder)
+
+        var companies = db.GetData();
+        foreach (Virksomhed model in companies)
         {
-            side.Add(model);
+            listPage.Add(model);
         }
 
         //todo: implementer databasehent
 
-        var vælgVirksomhed = side.Select();
+        var vælgVirksomhed = listPage.Select();
         if(vælgVirksomhed != null)
         {
-            Display(new VirksomhedSideTo(vælgVirksomhed));
+            Screen.Display(new VirksomhedSideTo(vælgVirksomhed));
         }
     }
-   
+    //    Der laves en skærm med en liste over virksomheder.
+    //Listen skal vise følgende kolonner
+    //• Firmanavn
+    //• Land
+    //• Valut
 }
