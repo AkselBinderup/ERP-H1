fbc208dcd3e515894154d56b41562af803774c43
victoref <victoref3@gmail.com>
2024-06-11 11:30:31 +0200

    Unit test slet

--- UnitTests/KundeTest/KundeGenerelTest.cs
+++ UnitTests/KundeTest/KundeGenerelTest.cs
@@ -7,13 +7,7 @@ public class KundeGenerelTest
     [Fact]
     public void KundeInfoScreenUnitTest()
     {
-        Adresse adresseEksempel = new Adresse(vejNavn: "Struervej", 70, "Aalborg", 9000);
-
-        Kunde jon = new Kunde { KundeNummer = 1, Fornavn = "Jon", Efternavn = "Hedegaard", TelefonNummer = 88888888, Email = "Jon@gmail.com", Adresse = adresseEksempel, SidsteKøb = DateTime.Now };
-
-        KundeInfoScreen kunde = new KundeInfoScreen(jon);
-
-        Assert.True(kunde.Title == "Detaljer for " + jon.FuldeNavn) ;
+        
     }
     [Fact]
     public void KundeListeUnitTest()
